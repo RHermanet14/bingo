7a48ce3ea0be83633309c068643b2122022cfff7
RHermanet14 <rhermanet14@gmail.com>
2026-03-20 02:17:17 -0400

    started routing and username validation

---
+++ app/browser/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="min-h-screen bg-gray-200 flex flex-col items-center justify-center">
+      <h1 className="text-4xl mb-6">Lobby Browser WOP</h1>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,32 @@
+"use client";
+import { useRouter } from "next/navigation";
+import {useState} from "react"
+
 export default function Page() {
+  const [username, setUsername] = useState("");
+  const router = useRouter();
+  const handleSubmit = () => {
+    if (username.trim().length < 1 || username.trim().length > 9) {
+      alert("Invalid username");
+      return
+    }
+    localStorage.setItem("username", username); // change to middleware
+    router.push("/browser");
+  };
+
   return (
     <div className="min-h-screen bg-gray-200 flex flex-col items-center justify-center">
       <h1 className="text-4xl mb-6">Enter Username</h1>
       <form action="/search" className="flex space-x-2">
         <input
-          name="query"
-          type="text"
-          placeholder="Search"
+          name="name"
+          placeholder="friendly_user123"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
           className="border border-gray-400 rounded px-3 py-1"
         />
         <button
-          type="submit"
+          onClick={handleSubmit}
           className="bg-blue-600 text-white rounded px-4 py-1 hover:bg-blue-700"
         >
           Submit
