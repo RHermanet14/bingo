e579f69aaa4f81e6f88329890f3696685a8aba25
RHermanet14 <rhermanet14@gmail.com>
2026-03-23 12:48:49 -0400

    lobby creator details

---
+++ app/lobby/grid/types.ts
@@ -0,0 +1,4 @@
+export type LobbyRow = {
+    id: number;
+    name: string;
+};
--- app/lobby/page.tsx
+++ app/lobby/page.tsx
@@ -1,14 +1,28 @@
+"use client";
+import Grid from "./grid/Grid";
+import {LobbyRow} from "./grid/types"
+import { useRouter } from "next/navigation";
+
 export default function LobbyPage() {
+    const router = useRouter();
+      const items: LobbyRow[] = [
+        {id: 1, name:"john"},
+        {id: 2, name:"kelly"},
+        {id: 3, name: "crazypineapple808974"},
+      ];
+    
     return (
         <div>
             <h1 className="text-2xl text-center p-1 bg-gray-400">Lobby Code: 123456</h1>
-            <div className="p-2 flex gap-4 text-lg">
-                <button className="bg-blue-400">Back To Browser</button>
+            <div className="bg-gray-200">
+                <div className="p-2 flex gap-4 text-lg">
+                <button onClick={() => router.replace("/browser")} className="bg-blue-400">Back To Browser</button>
                 <p className="ml-auto">1/30</p>
                 <button className="bg-blue-400">Start Game</button>
-            </div>
-            <div className="min-h-screen bg-gray-200 flex flex-col items-center justify-center">
-                <h1 className="text-4xl mb-6">Username and button to kick player</h1>
+                </div>
+                    <div className="min-h-screen flex flex-col items-center justify-center">
+                    <Grid items={items}/>
+                </div>
             </div>
         </div>
     )
