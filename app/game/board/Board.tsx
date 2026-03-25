3971f8231f89f4290a607944d4e4ed8df0ce556b
RHermanet14 <rhermanet14@gmail.com>
2026-03-25 19:36:45 -0400

    added chip placement

--- app/game/board/Board.tsx
+++ app/game/board/Board.tsx
@@ -1,31 +1,33 @@
 "use client";
+import { useState } from "react";
 
-const getBingoNumber = (letter: number): number => {
-        switch(letter) {
-            case 0:
-                return Math.floor(Math.random() * 15) + 1;
-            case 1:
-                return Math.floor(Math.random() * 15) + 15;
-            case 2:
-                return Math.floor(Math.random() * 15) + 30;
-            case 3:
-                return Math.floor(Math.random() * 15) + 45;
-            case 4:
-                return Math.floor(Math.random() * 15) + 60;
-            default:
-                return -1;
-        }
-    };
+const numArr: number[] = [];
+
+function getBingoNumber(letter: number): number {
+    return Math.floor(Math.random() * 15) + (letter * 15) + 1;
+};
+
+function buildBoard(index: number): number {
+        let space: number = getBingoNumber(index%5);
+        while(numArr.includes(space)) {space = getBingoNumber(index%5);}
+        numArr.push(space);
+        return space;
+}
 
 export default function Board() {
-    const board = [];
-    const numArr: number[] = [];
-    for (let i: number = 0; i < 25; i++) {
-        let space: number = getBingoNumber(i%5);
-        while(numArr.includes(space)) {space = getBingoNumber(i%5);}
-        board.push(<div key={i} onClick={() => alert(`putting chip down on space`)}>{space}</div>);
-        numArr.push(space);
-    }
+    const [active, setActive] = useState<boolean[]>(
+        () => Array.from({ length: 25 }, () => false)
+    );
+    
+    const [bingoNumbers] = useState<number[]>(
+        () => Array.from({length: 25}, (_, i) => buildBoard(i))
+    );
+
+    
+    const placeChip = (index: number) => {
+        setActive(prev => prev.map((val, i) => (i === index ? !val : val)));
+    };
+    
 
     return (
         <div className="grid grid-cols-5 gap-5 text-center text-3xl border-2 p-4 bg-gray-400">
@@ -34,7 +36,15 @@ export default function Board() {
             <p>N</p>
             <p>G</p>
             <p>O</p>
-            {board}
+            {bingoNumbers.map((num,i) => (
+                    <button
+                        key={i}
+                        onClick={() => placeChip(i)}
+                        className={active[i] ? "bg-yellow-300 opacity-75" : "bg-gray-400"}
+                    >
+                        {i === 12 ? "Free Space" : num}
+                    </button>
+            ))}
         </div>
     );
 }
