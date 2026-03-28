6b1006c4eda904480fc280af12f161864026d071
RHermanet14 <rhermanet14@gmail.com>
2026-03-28 00:45:14 -0400

    finished bingo number getter

--- app/game/timer/Timer.tsx
+++ app/game/timer/Timer.tsx
@@ -1,6 +1,10 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 
 const generatedNumbers: number[] = [];
+
+function getRandomBingoNumber(): number {
+    return Math.floor(Math.random() * 75) + 1;
+}
 
 function convertToBingoNumber(num: number): string {
     const numString: string = num.toString();
@@ -15,20 +19,27 @@ function convertToBingoNumber(num: number): string {
     else
         return "O" + numString;
 }
-function getRandomBingoNumber() {
-    generatedNumbers.push(5);
-}
 
 export default function Timer() {
+    const [letterNum, setLetterNum] = useState("");
+
+    function setBingoNumber() {
+        const num: number = getRandomBingoNumber();
+        generatedNumbers.push(num);
+        const str: string = convertToBingoNumber(num);
+        setLetterNum(str);
+    }
+
     useEffect(() => {
-        const interval = setInterval(getRandomBingoNumber, 1000);
+        const interval = setInterval(setBingoNumber, 5000);
         return () => {
             clearInterval(interval);
         };
     }, []);
+    
     return (
         <div>
-            <h1 className="text-4xl mb-6">{generatedNumbers}</h1>
+            <h1 className="text-4xl mb-6">{letterNum}</h1>
         </div>
     );
 }
