13d11f0fed5f82b02bc10518add09e15127b35a1
RHermanet14 <rhermanet14@gmail.com>
2026-05-01 21:19:02 -0400

    fixed error

--- app/lobby/[id]/page.tsx
+++ app/lobby/[id]/page.tsx
@@ -2,7 +2,7 @@ import {cookies} from "next/headers";
 import LobbyList from "./LobbyList";
 
 export default async function LobbyPage() {
-    const username = (await cookies()).get("username")?.value;
+    const username = (await cookies()).get("username")?.value ?? "unknown";
   return (
     <LobbyList username={username}/>
   );
