d98537206a083f84ff2d7c05ce3b781173b88b95
RHermanet14 <rhermanet14@gmail.com>
2026-05-03 21:12:27 -0400

    actually made the refresh button work

--- app/browser/page.tsx
+++ app/browser/page.tsx
@@ -2,20 +2,21 @@
 import Grid from "./grid/Grid";
 import {BrowserRow} from "./grid/types"
 import { useRouter } from "next/navigation";
-import {useEffect, useState} from "react"
+import {useCallback, useEffect, useState} from "react"
 import {supabase} from "@/lib/supabase"
 
 export default function BrowserPage() {
   const router = useRouter();
   const [rooms, setRooms] = useState<BrowserRow[]>([]);
-  
-  useEffect(() => {
-    const fetchRooms = async() => {
+
+  const fetchRooms = useCallback(async(): Promise<BrowserRow[]> => {
       const {data} = await supabase.from("rooms").select("*");
-      setRooms(data || []);
-    };
-    fetchRooms();
+      return data ?? [];
   }, []);
+
+  useEffect(() => {
+    fetchRooms().then(setRooms);
+  }, [fetchRooms]);
 
   return (
     <div>
@@ -23,7 +24,10 @@ export default function BrowserPage() {
       <div className="flex bg-gray-400 p-1 gap-2">
         <p>Search</p>
         <input placeholder="Enter Lobby Code" className="bg-white"/>
-        <button className="bg-gray-500 rounded" onClick={() => router.refresh()}>Refresh</button>
+        <button className="bg-gray-500 rounded" onClick={async() => {
+          const newRooms: BrowserRow[] = await fetchRooms();
+          setRooms(newRooms);
+        }}>Refresh</button>
         <button onClick={() => router.push("/lobby")}
         className="ml-auto bg-gray-500 rounded">Create Lobby</button>
       </div>
