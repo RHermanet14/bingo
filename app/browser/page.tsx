555da85652a44432aa08c813af360c8d06c5d74b
RHermanet14 <rhermanet14@gmail.com>
2026-05-25 20:24:35 -0400

    adding settings and search

--- app/browser/page.tsx
+++ app/browser/page.tsx
@@ -20,18 +20,27 @@ export default function BrowserPage() {
     fetchRooms().then(setRooms);
   }, [fetchRooms]);
 
+  const searchRooms = () => {
+    
+  }
+
   return (
     <div>
       <h1 className="text-2xl text-center p-1 bg-gray-400">Browse Lobbies</h1>
       <div className="flex bg-gray-400 p-1 gap-2">
-        <p>Search</p>
+        <button onClick={() => searchRooms()}className="bg-orange-400 rounded">Search</button>
         <input placeholder="Enter Lobby Code" className="bg-white"/>
         <button className="bg-gray-500 rounded" onClick={async() => {
           const newRooms: BrowserRow[] = await fetchRooms();
           setRooms(newRooms);
         }}>Refresh</button>
-        <button onClick={() => router.push("/lobby")}
-        className="ml-auto bg-gray-500 rounded">Create Lobby</button>
+        <div className="flex ml-auto gap-2">
+          <button onClick={() => router.push("/settings")} className="bg-gray-500 rounded">Settings</button>
+          <button onClick={() => router.push("/lobby")}
+        className="bg-gray-500 rounded">Create Lobby</button>
+        </div>
+        
+        
       </div>
       <Grid items={rooms}/>
     </div>
