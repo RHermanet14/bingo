ab5bcb0533a8b83c39e9f14e05e613f38cd0c136
RHermanet14 <rhermanet14@gmail.com>
2026-05-27 21:41:15 -0400

    fixed search and made it work

--- app/browser/page.tsx
+++ app/browser/page.tsx
@@ -7,6 +7,7 @@ import {useCallback, useEffect, useState} from "react"
 export default function BrowserPage() {
   const router = useRouter();
   const [rooms, setRooms] = useState<BrowserRow[]>([]);
+  const [search, setSearch] = useState<string>("");
 
   const fetchRooms = useCallback(async(): Promise<BrowserRow[]> => {
       const res = await fetch("/api/browser", {
@@ -20,8 +21,15 @@ export default function BrowserPage() {
     fetchRooms().then(setRooms);
   }, [fetchRooms]);
 
-  const searchRooms = () => {
-
+  const searchRooms = async() => {
+    const params = new URLSearchParams({
+      name: search
+    });
+    const res = await fetch(`/api/browser?${params.toString()}`, {
+      method:"GET"
+    });
+    const data = await res.json();
+    setRooms(data);
   }
   
   return (
@@ -29,10 +37,16 @@ export default function BrowserPage() {
       <h1 className="text-2xl text-center p-1 bg-gray-400">Browse Lobbies</h1>
       <div className="flex bg-gray-400 p-1 gap-2">
         <button onClick={() => searchRooms()}className="bg-orange-400 rounded">Search</button>
-        <input placeholder="Enter Lobby Code" className="bg-white"/>
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search Host Name"
+          className="bg-white"
+        />
         <button className="bg-gray-500 rounded" onClick={async() => {
           const newRooms: BrowserRow[] = await fetchRooms();
           setRooms(newRooms);
+          setSearch("");
         }}>Refresh</button>
         <div className="flex ml-auto gap-2">
           <button onClick={() => router.push("/settings")} className="bg-gray-500 rounded">Settings</button>
